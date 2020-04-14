7920b9a9d299c37d3c7ff00c809ecd54e293e127
Yury Sergeyev <60914627+Yorksergeyev@users.noreply.github.com>
2020-04-14 21:10:31 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+
 module.exports = {
   mode: 'development',
   entry: './bot.js/troll.js', //указать наш файл
@@ -10,22 +11,3 @@ module.exports = {
   watch: true
 };
 
-module.exports = {
-  mode: 'development',
-  entry: './bot.js/boss.js', //указать наш файл
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'boss.bundle.js' //указать итоговый файл
-  },
-  watch: true
-};
-
-module.exports = {
-  mode: 'development',
-  entry: './bot.js/tank.js', //указать наш файл
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'tank.bundle.js' //указать итоговый файл
-  },
-  watch: true
-};
